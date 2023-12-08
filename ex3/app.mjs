6bf5e86f693bcfda8f9c117d664a5b3ec7601f10
amzxir <amirreza79ahmadi@gamil.com>
2023-12-08 20:25:53 +0330

    sad

--- ex3/app.mjs
+++ ex3/app.mjs
@@ -1,9 +1,17 @@
 import myModule from './module.cjs';
+import { createRequire } from 'module';
 
 const log = console.log;
+
+const require = createRequire(import.meta.url);
+
+const json = require('./data.json');
 
 log(myModule.next());
 log(myModule.next());
 log(myModule.next());
 log(myModule.prev());
 log(myModule.square());
+
+
+log(json)
